f372ce9e3e5a9b6a61ff3681aa8c9d5e3bf36e24
DESKTOP-8SO1TO1\user <1025candy@naver.com>
2018-06-09 21:19:02 +0900

    String background SET

--- 알고리즘 설계 프로젝트/알고리즘 설계 프로젝트/string.cpp
+++ 알고리즘 설계 프로젝트/알고리즘 설계 프로젝트/string.cpp
@@ -1,3 +1,72 @@
 #pragma once
 #include "stdafax.h"
 
+String::String()
+{
+	string tempLine = "";
+	int tempLen = 0;
+	int stringIndex = 0;
+
+	ifstream input1("subject.txt");
+
+	getline(input1, tempLine);
+	this->nodeSize = stoi(tempLine);
+	stringNode = new node[nodeSize];
+
+	stringSize = 7 * nodeSize + 1;
+	subjectNums = new char[stringSize];
+
+	for (int i = 0; i < nodeSize; i++)
+	{
+		getline(input1, tempLine);
+		tempLen = tempLine.length();
+		stringNode[i].name = new char[tempLen];
+		strncpy(stringNode[i].name, tempLine.c_str(), tempLen);
+		stringNode[i].numName = tempLen;
+
+		getline(input1, tempLine);
+		if (tempLine == "")stringNode[i].numPreName = 0;
+		else
+		{
+			tempLen = tempLine.length();
+			stringNode[i].preSubject = new char[tempLen];
+			strncpy(stringNode[i].preSubject, tempLine.c_str(), tempLen);
+			stringNode[i].numPreName = tempLen;
+		}
+
+		getline(input1, tempLine);
+		stringNode[i].subjectNum = new char[7];
+		strncpy(stringNode[i].subjectNum, tempLine.c_str(), 7);
+
+		for (int k = 0; k < 7; k++)
+			subjectNums[stringIndex++] = stringNode[i].subjectNum[k];
+
+		getline(input1, tempLine);
+		stringNode[i].isEssential = stoi(tempLine);
+
+		getline(input1, tempLine);
+		stringNode[i].isDesign = stoi(tempLine);
+
+		getline(input1, tempLine);
+		stringNode[i].isMajor = stoi(tempLine);
+
+		getline(input1, tempLine);
+		stringNode[i].credit = stoi(tempLine);
+
+	}
+	input1.close();
+	//showString();
+
+
+}
+String::~String()
+{
+	delete[]stringNode;
+	delete[]subjectNums;
+ }
+
+void String::showString()
+{
+	for (int i = 0; i < stringSize; i++)
+		cout << subjectNums[i];
+}
--- 알고리즘 설계 프로젝트/알고리즘 설계 프로젝트/string.h
+++ 알고리즘 설계 프로젝트/알고리즘 설계 프로젝트/string.h
@@ -10,7 +10,12 @@ public:
 	~String();
 	void showString();
 
+
 private:
+	node * stringNode;     //string에서 과목을 저장할 배열
+	char * subjectNums;    //string에서 과목을 탐색할 과목명 문자열
+	int nodeSize;          //stringNode의 사이즈
+	int stringSize;        //subjectNums의 사이즈
 
 
 };
